c3c875cc86452734b215f71fb473f9f686321504
Maurycy K <msjspurs@gmail.com>
2026-07-17 13:56:17 +0200

    test: rename title test to be more descriptive

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("should have correct page title 'Rolnopol'", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Rolnopol");
 });
